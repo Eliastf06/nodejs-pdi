a93fb936730a713bf7936e94a5523c601f870ba6
Elias Franco <tizieli3@gmail.com>
2025-07-10 20:56:16 -0300

    Update index.js

--- params/index.js
+++ params/index.js
@@ -23,13 +23,11 @@ app.delete('/proyectos/:id', (req, res) => {
   res.json({ message: `Esta es la ruta DELETE de mi entidad proyectos con ID: ${id}` });
 });
 
-// --- Ruta Adicional GET / ---
 
 app.get('/', (req, res) => {
   res.json({ message: 'Bienvenido a la API de mi_proyecto' });
 });
 
-// --- Iniciar el Servidor ---
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor Express corriendo en http://localhost:${PORT}`);
